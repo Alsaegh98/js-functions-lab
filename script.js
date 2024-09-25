49c080e2e1bbabefa91aae22d56c709ceea5e375
User Name <user@email.com>
2024-09-25 11:29:55 +0300

    exercise fineshed

--- script.js
+++ script.js
@@ -47,3 +47,95 @@ else if (char == 'u'){
 }
 
 console.log('Exercise 3 Result:', isCharAVowel("a"));
+
+//////////////////// exercise 4
+
+function generateEmail( name, domain ) {
+  return `${name}@${domain}`
+
+  
+}
+console.log('Exercise 4 Result:', generateEmail("johnsmith", "example.com"));
+////////////////exercise 5
+
+function greetUser( name, time ) {
+  return `good ${time} ${name}`
+
+  
+}
+
+ 
+console.log('Exercise 5 Result:', greetUser("Sam", "morning"));
+
+
+///////////////// exercise 6
+
+
+const maxOfThree= ( x , y , z ) => {
+
+  if (x < y){
+    return y;
+  }
+else if (x < z){
+  return z;
+}
+else if (y < z){
+  return z;
+}
+
+else if (z < y){
+  return y;
+}
+else
+return x;
+}
+ 
+console.log('Exercise 6 Result:', maxOfThree(5, 10, 8));
+
+////////////////////////exercise 7
+
+function calculateTip ( bill , percentage ) {
+
+return bill * percentage / 100;
+
+}
+console.log('Exercise 7 Result:', calculateTip(50, 20));
+
+
+///////////////// exercise 8
+
+function convertTemperature( temperature , scale ){
+
+if (scale === 'C'){
+  return (temperature * (9/5)+32);
+}
+  else{
+    return (temperature - 32);
+  }
+
+}
+console.log('Exercise 8 Result:', convertTemperature(32, "C"));
+
+
+//////////// exercise 9
+
+function basicCalculator( x , y , string ){
+
+  if (string === 'subtract'){
+    return x - y;
+  }
+  else if (string === 'add'){
+    return x + y;
+  }
+
+  else if (string === 'multiply'){
+    return x * y;
+  }
+
+  else if (string === 'divide'){
+    return x / y;
+  }
+  
+}
+
+console.log('Exercise 9 Result:', basicCalculator(10, 5, "subtract"));
